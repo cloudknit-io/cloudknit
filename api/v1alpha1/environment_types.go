aab41f4e294f8a6213cf993e8b196d065c0a1a00
Adarsh Shah <shahadarsh@gmail.com>
2020-11-02 13:53:45 -0500

    initial commit for environment-operator

---
+++ api/v1alpha1/environment_types.go
@@ -0,0 +1,73 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type Module struct {
+	Source string `json:"source"`
+	Path   string `json:"path"`
+}
+
+type Variable struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type TerraformConfig struct {
+	Name      string      `json:"name"`
+	Module    *Module     `json:"module"`
+	Variables []*Variable `json:"variables,omitempty"`
+}
+
+// EnvironmentSpec defines the desired state of Environment
+type EnvironmentSpec struct {
+	TerraformConfigs []*TerraformConfig `json:"terraformConfigs"`
+}
+
+// EnvironmentStatus defines the observed state of Environment
+type EnvironmentStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Environment is the Schema for the environments API
+type Environment struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   EnvironmentSpec   `json:"spec,omitempty"`
+	Status EnvironmentStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// EnvironmentList contains a list of Environment
+type EnvironmentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Environment `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Environment{}, &EnvironmentList{})
+}
