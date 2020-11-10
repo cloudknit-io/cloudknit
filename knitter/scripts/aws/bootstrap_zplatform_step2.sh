6a881eeb88ca29c8028db18d95359cc8534bdb93
Adarsh Shah <shahadarsh@gmail.com>
2020-11-10 02:35:06 -0500

    replacing infra-deplpy-config repo with terraform-environment for argocd apps

--- knitter/scripts/aws/bootstrap_zplatform_step2.sh
+++ knitter/scripts/aws/bootstrap_zplatform_step2.sh
@@ -7,7 +7,7 @@ sleep 2m
 
 argocd login --insecure localhost:8080 --grpc-web --username admin --password $argocd_server_name
 
-argocd repo add --name infra-deploy-terraform-config git@github.com:CompuZest/infra-deploy-terraform-config.git --ssh-private-key-path argo --insecure-ignore-host-key
+argocd repo add --name terraform-environment git@github.com:CompuZest/terraform-environment.git --ssh-private-key-path argo --insecure-ignore-host-key
 
 argocd repo add --name helm-charts git@github.com:CompuZest/helm-charts.git --ssh-private-key-path argo --insecure-ignore-host-key
 
@@ -19,7 +19,7 @@ cd ../../../infra-deploy-bootstrap/scripts
 kubectl apply -f terraform-sync-template.yaml
 kubectl apply -f terraform-template.yaml
 
-argocd app create 1-customer --repo git@github.com:CompuZest/infra-deploy-terraform-config.git --path 1 --dest-server https://kubernetes.default.svc --dest-namespace default --sync-policy automated --auto-prune
+argocd app create 1-customer --repo git@github.com:CompuZest/terraform-environment.git --path 1 --dest-server https://kubernetes.default.svc --dest-namespace default --sync-policy automated --auto-prune
 
 kubectl port-forward service/argo-workflow-server 8081:2746 -n argo &
 
