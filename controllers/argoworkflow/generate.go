5fd39402194099e498f40cbdf928a2ab17371a91
Adarsh Shah <shahadarsh@gmail.com>
2020-12-10 23:23:36 -0500

    support for dependencies in WOfW

--- controllers/argoworkflow/generate.go
+++ controllers/argoworkflow/generate.go
@@ -10,63 +10,56 @@ func GenerateWorkflowOfWorkflows(environment stablev1alpha1.Environment) *workfl
 
 	workflowTemplate := "terraform-sync-template"
 
-	var parallelSteps []workflow.ParallelSteps
-
-	custEnvPrefix := environment.Spec.TeamName + "-" + environment.Spec.EnvName + "-"
+	var tasks []workflow.DAGTask
 
 	for _, terraformConfig := range environment.Spec.TerraformConfigs {
 
-		workflowName := custEnvPrefix + terraformConfig.ConfigName
-
-		step := workflow.ParallelSteps{
-			Steps: []workflow.WorkflowStep{
-				{
-					Name: workflowName,
-					TemplateRef: &workflow.TemplateRef{
-						Name:     "workflow-trigger-template",
-						Template: "run",
+		task := workflow.DAGTask{
+			Name: terraformConfig.ConfigName,
+			TemplateRef: &workflow.TemplateRef{
+				Name:     "workflow-trigger-template",
+				Template: "run",
+			},
+			Dependencies: terraformConfig.DependsOn,
+			Arguments: workflow.Arguments{
+				Parameters: []workflow.Parameter{
+					{
+						Name:  "workflowtemplate",
+						Value: &workflowTemplate,
 					},
-					Arguments: workflow.Arguments{
-						Parameters: []workflow.Parameter{
-							{
-								Name:  "workflowtemplate",
-								Value: &workflowTemplate,
-							},
-							{
-								Name:  "module_source",
-								Value: &terraformConfig.Module.Source,
-							},
-							{
-								Name:  "module_source_path",
-								Value: &terraformConfig.Module.Path,
-							},
-							{
-								Name:  "variables_file_source",
-								Value: &terraformConfig.VariablesFile.Source,
-							},
-							{
-								Name:  "variables_file_path",
-								Value: &terraformConfig.VariablesFile.Path,
-							},
-							{
-								Name:  "team_name",
-								Value: &environment.Spec.TeamName,
-							},
-							{
-								Name:  "env_name",
-								Value: &environment.Spec.EnvName,
-							},
-							{
-								Name:  "config_name",
-								Value: &terraformConfig.ConfigName,
-							},
-						},
+					{
+						Name:  "module_source",
+						Value: &terraformConfig.Module.Source,
+					},
+					{
+						Name:  "module_source_path",
+						Value: &terraformConfig.Module.Path,
+					},
+					{
+						Name:  "variables_file_source",
+						Value: &terraformConfig.VariablesFile.Source,
+					},
+					{
+						Name:  "variables_file_path",
+						Value: &terraformConfig.VariablesFile.Path,
+					},
+					{
+						Name:  "team_name",
+						Value: &environment.Spec.TeamName,
+					},
+					{
+						Name:  "env_name",
+						Value: &environment.Spec.EnvName,
+					},
+					{
+						Name:  "config_name",
+						Value: &terraformConfig.ConfigName,
 					},
 				},
 			},
 		}
 
-		parallelSteps = append(parallelSteps, step)
+		tasks = append(tasks, task)
 	}
 
 	return &workflow.Workflow{
@@ -90,8 +83,10 @@ func GenerateWorkflowOfWorkflows(environment stablev1alpha1.Environment) *workfl
 			Entrypoint: "main",
 			Templates: []workflow.Template{
 				{
-					Name:  "main",
-					Steps: parallelSteps,
+					Name: "main",
+					DAG: &workflow.DAGTemplate{
+						Tasks: tasks,
+					},
 				},
 			},
 		},
