a82687dad96f3ed4d67ab6c8d13c3e012a32ee06
Adarsh Shah <shahadarsh@gmail.com>
2020-12-10 23:27:34 -0500

    fix for new namespace argocd

--- knitter/scripts/terraform/send_slack_notification.sh
+++ knitter/scripts/terraform/send_slack_notification.sh
@@ -8,10 +8,12 @@ config_name=$4
 
 team_env_config_name=$team_name-$env_name-$config_name
 
-message="${team_env_config_name} terraform is out of sync. To see the diff & approve the sync to desired state go here: http://localhost:8081/workflows/argo/${workflow_name}"
+namespace="argocd"
+
+message="${team_env_config_name} terraform is out of sync. To see the diff & approve the sync to desired state go here: http://localhost:8081/workflows/${namespace}/${workflow_name}"
 
 data='{"channel": "slack-notification","message": "'$message'"}'
 echo $data
 
-curl -d "${data}" -H "Content-Type: application/json" -X POST http://webhook-eventsource-svc.argo.svc.cluster.local:12000/terraform-diff
+curl -d "${data}" -H "Content-Type: application/json" -X POST http://webhook-eventsource-svc.${namespace}.svc.cluster.local:12000/terraform-diff
 
