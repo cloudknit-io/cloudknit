6d324358ade1e1b7d1dd683a2cccff3085964bbc
Adarsh Shah <shahadarsh@gmail.com>
2021-01-05 17:38:20 -0500

    Added license info to files before porting over

--- knitter/bootstrap/aws/destroy_zLifecycle.sh
+++ knitter/bootstrap/aws/destroy_zLifecycle.sh
@@ -1,3 +1,14 @@
+# Copyright (C) 2020 CompuZest, Inc. - All Rights Reserved
+#
+# Unauthorized copying of this file, via any medium, is strictly prohibited
+# Proprietary and confidential
+#
+# NOTICE: All information contained herein is, and remains the property of
+# CompuZest, Inc. The intellectual and technical concepts contained herein are
+# proprietary to CompuZest, Inc. and are protected by trade secret or copyright
+# law. Dissemination of this information or reproduction of this material is
+# strictly forbidden unless prior written permission is obtained from CompuZest, Inc.
+
 argocd app delete 1-customer
 #argocd cluster rm arn:aws:eks:us-east-1:413422438110:cluster/0-sandbox-eks
 argocd repo rm git@github.com:CompuZest/infra-deploy-terraform-config.git
--- knitter/bootstrap/local/destroy_zLifecycle.sh
+++ knitter/bootstrap/local/destroy_zLifecycle.sh
@@ -1,3 +1,14 @@
+# Copyright (C) 2020 CompuZest, Inc. - All Rights Reserved
+#
+# Unauthorized copying of this file, via any medium, is strictly prohibited
+# Proprietary and confidential
+#
+# NOTICE: All information contained herein is, and remains the property of
+# CompuZest, Inc. The intellectual and technical concepts contained herein are
+# proprietary to CompuZest, Inc. and are protected by trade secret or copyright
+# law. Dissemination of this information or reproduction of this material is
+# strictly forbidden unless prior written permission is obtained from CompuZest, Inc.
+
 kubectl port-forward service/argo-cd-argocd-server 8080:80 -n argocd
 
 kubectl get applications -n argocd -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | xargs kubectl patch applications  -p '{"metadata":{"finalizers":[]}}' --type=merge -n argocd
