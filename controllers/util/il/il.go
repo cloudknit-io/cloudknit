44c6052951e50de9254a5695243afa1b50175c98
Arielle Sullivan <ariellejsullivan@gmail.com>
2021-02-10 23:18:41 -0800

    Hard code company repo

--- controllers/util/il/il.go
+++ controllers/util/il/il.go
@@ -3,9 +3,11 @@ package il
 type config struct {
 	TeamDirectory          string
 	ConfigWatcherDirectory string
+	CompanyConfigRepo      string
 }
 
 var Config = config{
 	TeamDirectory:          "team",
 	ConfigWatcherDirectory: "config-watcher",
+	CompanyConfigRepo:      "git@github.com:CompuZest/zmart-config.git",
 }
