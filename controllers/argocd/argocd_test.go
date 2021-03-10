00a780be385d13d207e31f4739f38ee799a4d742
Dejan Zele Pejchev <dpejcev@fevo.com>
2021-03-11 00:26:56 +0100

    Fix typo in test

--- controllers/argocd/argocd_test.go
+++ controllers/argocd/argocd_test.go
@@ -24,7 +24,7 @@ func TestRegisterRepoNewRepo(t *testing.T) {
 	r2    := ioutil.NopCloser(bytes.NewReader([]byte{}))
 	mockArgocdAPI.EXPECT().CreateRepository(gomock.Any(), gomock.Any(), gomock.Any()).Return(&http.Response{Body: r2}, nil)
 
-	log := ctrl.Log.WithName("TestRegisterRepoHappyPath")
+	log := ctrl.Log.WithName("TestRegisterRepoNewRepo")
 	registered, err := RegisterRepo(log, mockArgocdAPI, RepoOpts{})
 	assert.True(t, registered)
 	assert.NoError(t, err)
@@ -42,7 +42,7 @@ func TestRegisterRepoExistingRepo(t *testing.T) {
 	r1    := ioutil.NopCloser(bytes.NewReader([]byte{}))
 	mockArgocdAPI.EXPECT().ListRepositories(gomock.Any(), gomock.Any()).Return(&list, &http.Response{Body: r1}, nil)
 
-	log := ctrl.Log.WithName("TestRegisterRepoHappyPath")
+	log := ctrl.Log.WithName("TestRegisterRepoExistingRepo")
 	registered, err := RegisterRepo(log, mockArgocdAPI, RepoOpts{RepoUrl: repo.Repo})
 	assert.False(t, registered)
 	assert.NoError(t, err)
