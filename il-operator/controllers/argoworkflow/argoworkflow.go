55644c701694fcd77a1cd4925539d8b597ff3932
Dejan Zele Pejchev <dejan@compuzest.com>
2021-06-01 23:23:40 +0200

    Revert changes to argoworkflows

--- il-operator/controllers/argoworkflow/argoworkflow.go
+++ il-operator/controllers/argoworkflow/argoworkflow.go
@@ -94,93 +94,13 @@ func GenerateWorkflowOfWorkflows(environment stablev1alpha1.Environment) *workfl
 
 func GenerateLegacyWorkflowOfWorkflows(environment stablev1alpha1.Environment) *workflow.Workflow {
 
-	envComponentDirectory := il.EnvironmentComponentDirectory(environment.Spec.TeamName, environment.Spec.EnvName)
 	workflowTemplate := "terraform-sync-template"
-	tf := terraformgenerator.TerraformGenerator{}
 
 	var tasks []workflow.DAGTask
 
 	for _, environmentComponent := range environment.Spec.EnvironmentComponent {
 		moduleSource := il.EnvComponentModuleSource(environmentComponent.Module.Source, environmentComponent.Module.Name)
 		modulePath := il.EnvComponentModulePath(environmentComponent.Module.Path)
-		tfPath := tf.GenerateTerraformIlPath(envComponentDirectory, environmentComponent.Name)
-
-		parameters := []workflow.Parameter{
-			{
-				Name:  "workflowtemplate",
-				Value: &workflowTemplate,
-			},
-			{
-				Name:  "module_source",
-				Value: &moduleSource,
-			},
-			{
-				Name:  "module_source_path",
-				Value: &modulePath,
-			},
-			{
-				Name:  "team_name",
-				Value: &environment.Spec.TeamName,
-			},
-			{
-				Name:  "env_name",
-				Value: &environment.Spec.EnvName,
-			},
-			{
-				Name:  "config_name",
-				Value: &environmentComponent.Name,
-			},
-			{
-				Name:  "il_repo",
-				Value: &env.Config.ILRepoURL,
-			},
-			{
-				Name:  "terraform_il_path",
-				Value: &tfPath,
-			},
-		}
-
-		parameters := []workflow.Parameter{
-			{
-				Name:  "workflowtemplate",
-				Value: &workflowTemplate,
-			},
-			{
-				Name:  "module_source",
-				Value: &moduleSource,
-			},
-			{
-				Name:  "module_source_path",
-				Value: &modulePath,
-			},
-			{
-				Name:  "team_name",
-				Value: &environment.Spec.TeamName,
-			},
-			{
-				Name:  "env_name",
-				Value: &environment.Spec.EnvName,
-			},
-			{
-				Name:  "config_name",
-				Value: &environmentComponent.Name,
-			},
-		}
-
-		vf := environmentComponent.VariablesFile
-		if vf != nil {
-			variablesFileParams := []workflow.Parameter{
-				{
-					Name:  "variables_file_source",
-					Value: &vf.Source,
-				},
-				{
-					Name:  "variables_file_path",
-					Value: &vf.Path,
-				},
-			}
-			parameters = append(parameters, variablesFileParams...)
-		}
 
 		task := workflow.DAGTask{
 			Name: environmentComponent.Name,
@@ -190,7 +110,40 @@ func GenerateLegacyWorkflowOfWorkflows(environment stablev1alpha1.Environment) *
 			},
 			Dependencies: environmentComponent.DependsOn,
 			Arguments: workflow.Arguments{
-				Parameters: parameters,
+				Parameters: []workflow.Parameter{
+					{
+						Name:  "workflowtemplate",
+						Value: &workflowTemplate,
+					},
+					{
+						Name:  "module_source",
+						Value: &moduleSource,
+					},
+					{
+						Name:  "module_source_path",
+						Value: &modulePath,
+					},
+					{
+						Name:  "variables_file_source",
+						Value: &environmentComponent.VariablesFile.Source,
+					},
+					{
+						Name:  "variables_file_path",
+						Value: &environmentComponent.VariablesFile.Path,
+					},
+					{
+						Name:  "team_name",
+						Value: &environment.Spec.TeamName,
+					},
+					{
+						Name:  "env_name",
+						Value: &environment.Spec.EnvName,
+					},
+					{
+						Name:  "config_name",
+						Value: &environmentComponent.Name,
+					},
+				},
 			},
 		}
 
