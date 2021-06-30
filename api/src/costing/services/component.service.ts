686eec14922dc01d3c16476271b7fdcd399f4087
Shashank <83878475+shashank-compuzest@users.noreply.github.com>
2021-06-30 18:37:59 +0530

    Soft delete (#25)
    * adding soft delete option
    
    * fetching those which are not deleted
    
    * Adding soft delete

--- api/src/costing/services/component.service.ts
+++ api/src/costing/services/component.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm'
-import { Subject } from 'rxjs'
-import { Component } from 'src/typeorm/costing/entities/Component'
-import { CostComponent, Resource } from 'src/typeorm/resources/Resource.entity'
-import { Connection, Repository } from 'typeorm'
-import { CostingDto } from '../dtos/Costing.dto'
-import { Mapper } from '../utilities/mapper'
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Subject } from "rxjs";
+import { Component } from "src/typeorm/costing/entities/Component";
+import { CostComponent, Resource } from "src/typeorm/resources/Resource.entity";
+import { Connection, Repository } from "typeorm";
+import { CostingDto } from "../dtos/Costing.dto";
+import { Mapper } from "../utilities/mapper";
 
 @Injectable()
 export class ComponentService {
-  readonly stream: Subject<{}> = new Subject<{}>()
-  readonly notifyStream: Subject<{}> = new Subject<{}>()
+  readonly stream: Subject<{}> = new Subject<{}>();
+  readonly notifyStream: Subject<{}> = new Subject<{}>();
   constructor(
     private readonly connection: Connection,
     @InjectRepository(Component)
@@ -18,67 +18,80 @@ export class ComponentService {
     @InjectRepository(Resource)
     private resourceRepository: Repository<Resource>,
     @InjectRepository(CostComponent)
-    private costComponentRepository: Repository<CostComponent>,
+    private costComponentRepository: Repository<CostComponent>
   ) {}
 
-  async getAll(): Promise<{}> {
+  async getAll(): Promise<Component[]> {
     const components = await this.componentRepository.find({
-      where : {
-        isDeleted : 0
-      }
-    })
-    this.stream.next(components)
-    return components
+      where: {
+        isDeleted: 0,
+      },
+    });
+    return components;
   }
   async getEnvironmentCost(
     teamName: string,
-    environmentName: string,
+    environmentName: string
   ): Promise<number> {
     const raw = await this.componentRepository
-      .createQueryBuilder('components')
-      .select('SUM(components.cost) as cost')
+      .createQueryBuilder("components")
+      .select("SUM(components.cost) as cost")
       .where(
-        `components.teamName = '${teamName}' and components.environmentName = '${environmentName}' and components.isDeleted = 0`,
+        `components.teamName = '${teamName}' and components.environmentName = '${environmentName}' and components.isDeleted = 0`
       )
-      .getRawOne()
-    return Number(raw.cost || 0)
+      .getRawOne();
+    return Number(raw.cost || 0);
   }
 
   async getComponentCost(componentId: string): Promise<number> {
     const raw = await this.componentRepository
-      .createQueryBuilder('components')
-      .select('SUM(components.cost) as cost')
+      .createQueryBuilder("components")
+      .select("SUM(components.cost) as cost")
       .where(`components.id = '${componentId}' and components.isDeleted = 0`)
-      .getRawOne()
-    return Number(raw.cost || 0)
+      .getRawOne();
+    return Number(raw.cost || 0);
   }
 
   async getTeamCost(name: string): Promise<number> {
     const raw = await this.componentRepository
-      .createQueryBuilder('components')
-      .select('SUM(components.cost) as cost')
+      .createQueryBuilder("components")
+      .select("SUM(components.cost) as cost")
       .where(`components.teamName = '${name}' and components.isDeleted = 0`)
-      .getRawOne()
-    return Number(raw.cost || 0)
+      .getRawOne();
+    return Number(raw.cost || 0);
   }
 
   async saveComponents(costing: CostingDto): Promise<boolean> {
-    const id = `${costing.teamName}-${costing.environmentName}-${costing.component.componentName}`
-    const component = new Component()
-    component.teamName = costing.teamName
-    component.environmentName = costing.environmentName
-    component.id = id
-    component.componentName = costing.component.componentName
-    component.cost = costing.component.cost
-    component.isDeleted = costing.component.isDeleted;
-    await this.componentRepository.delete({
-      id: id
+    const id = `${costing.teamName}-${costing.environmentName}-${costing.component.componentName}`;
+    let savedComponent = null;
+    if (costing.component.isDeleted) {
+      savedComponent = this.softDelete(id);
+    } else {
+      const component = new Component();
+      component.teamName = costing.teamName;
+      component.environmentName = costing.environmentName;
+      component.id = id;
+      component.componentName = costing.component.componentName;
+      component.cost = costing.component.cost;
+      component.isDeleted = costing.component.isDeleted;
+      await this.componentRepository.delete({
+        id: id,
+      });
+      savedComponent = await this.componentRepository.save(component);
+      const resources = await this.resourceRepository.save(
+        Mapper.mapToResourceEntity(component, costing.component.resources)
+      );
+      savedComponent.resources = resources;
+    }
+    this.notifyStream.next(savedComponent);
+    return true;
+  }
+
+  async softDelete(id: string) {
+    return await this.componentRepository.save({
+      id: id,
+      isDeleted: true,
     });
-    const savedComponent = await this.componentRepository.save(component);
-    const resources = await this.resourceRepository.save(Mapper.mapToResourceEntity(component, costing.component.resources));
-    savedComponent.resources = resources;
-    this.notifyStream.next(savedComponent)
-    return true
   }
 
   async getResourceData(id: string) {
@@ -86,27 +99,27 @@ export class ComponentService {
       where: {
         componentId: id,
       },
-    })
-    const roots = []
-    const resources = new Map<string, any>()
+    });
+    const roots = [];
+    const resources = new Map<string, any>();
     for (let i = 0; i < resultSet.length; i++) {
-      resultSet[i].subresources = []
-      const resource = resultSet[i]
+      resultSet[i].subresources = [];
+      const resource = resultSet[i];
       if (!resultSet[i].parentId) {
-        roots.push(resource)
-        resources.set(resource.id, resource)
+        roots.push(resource);
+        resources.set(resource.id, resource);
       } else {
-        resources.set(resource.id, resource)
-        resources.get(resource.parentId).subresources.push(resource)
+        resources.set(resource.id, resource);
+        resources.get(resource.parentId).subresources.push(resource);
       }
     }
     return {
       componentId: id,
       resources: roots,
-    }
+    };
   }
 
   async execute(query: string) {
-    return await this.connection.query(query)
+    return await this.connection.query(query);
   }
 }
