5373cbcd9c37be37437cc2744c01596b79b7db8f
Shashank Sharma <shashank9226@gmail.com>
2021-07-23 15:38:51 +0530

    Saving logs in reconcileid folder instead of plan

--- knitter/terraform-image/scripts/terraform_apply_plan.sh
+++ knitter/terraform-image/scripts/terraform_apply_plan.sh
@@ -1,15 +1,16 @@
 echo $show_output_start
-echo "Executing apply plan..."
+echo "Executing apply plan..." 2>&1 | tee /tmp/plan_output.txt
 echo $show_output_end
 data='{"metadata":{"labels":{"component_status":"running_plan"}}}'
 argocd app patch $team_env_config_name --patch $data --type merge >null
 
 echo $show_output_start
-terraform plan -lock=$lock_state -parallelism=2 -input=false -no-color -out=terraform-plan -detailed-exitcode
+terraform plan -lock=$lock_state -parallelism=2 -input=false -no-color -out=terraform-plan -detailed-exitcode 2>&1 | tee -a /tmp/plan_output.txt
 result=$?
 echo -n $result >/tmp/plan_code.txt
 echo $show_output_end
 
+aws s3 cp /tmp/plan_output.txt s3://zlifecycle-tfplan-zmart/$team_name/$env_name/$config_name/$config_reconcile_id/plan_output
 aws s3 cp terraform-plan s3://zlifecycle-tfplan-zmart/$team_name/$env_name/$config_name/$config_reconcile_id
 
 data='{"metadata":{"labels":{"component_status":"calculating_cost"}}}'
--- knitter/terraform-image/scripts/terraform_destroy.sh
+++ knitter/terraform-image/scripts/terraform_destroy.sh
@@ -1,5 +1,5 @@
 echo $show_output_start
-echo "Executing terraform destroy..."
+echo "Executing terraform destroy..." 2>&1 | tee /tmp/apply_output.txt
 echo $show_output_end
 data='{"metadata":{"labels":{"component_status":"destroying"}}}'
 
@@ -7,10 +7,12 @@ argocd app patch $team_env_config_name --patch $data --type merge >null
 aws s3 cp s3://zlifecycle-tfplan-zmart/$team_name/$env_name/$config_name/$config_reconcile_id terraform-plan
 
 echo $show_output_start
-terraform apply -auto-approve -input=false -parallelism=2 -no-color terraform-plan || Error "Can not apply terraform destroy"
+terraform apply -auto-approve -input=false -parallelism=2 -no-color terraform-plan || Error "Can not apply terraform destroy" 2>&1 | tee -a /tmp/apply_output.txt
 result=$?
 echo -n $result >/tmp/plan_code.txt
 echo $show_output_end
+
+aws s3 cp /tmp/apply_output.txt s3://zlifecycle-tfplan-zmart/$team_name/$env_name/$config_name/$config_reconcile_id/apply_output
 
 if [ $result -eq 0 ]; then
     data='{"metadata":{"labels":{"component_status":"destroyed"}}}'
