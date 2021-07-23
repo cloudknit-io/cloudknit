1ece79ee7736d47a25ec9510ca975e75af895480
Shashank <83878475+shashank-compuzest@users.noreply.github.com>
2021-07-23 20:22:04 +0530

    Hardcoding region

--- api/src/costing/utilities/s3Handler.ts
+++ api/src/costing/utilities/s3Handler.ts
@@ -13,7 +13,7 @@ export class S3Handler {
     this._s3 = new S3({
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION,
+      region: 'us-east-1',
     })
   }
 
