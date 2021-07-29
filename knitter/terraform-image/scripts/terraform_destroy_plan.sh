242ad60eea2e8c0a31f0df6829382283d971c8b4
Adarsh Shah <github@shahadarsh.com>
2021-07-29 16:12:13 -0400

    fix tfplans s3 path

--- knitter/terraform-image/scripts/terraform_destroy_plan.sh
+++ knitter/terraform-image/scripts/terraform_destroy_plan.sh
@@ -11,7 +11,7 @@ echo -n $result >/tmp/plan_code.txt
 echo $show_output_end
 
 #aws s3 cp /tmp/plan_output.txt s3://zlifecycle-tfplan-zmart/$team_name/$env_name/$config_name/$config_reconcile_id/plan_output
-aws s3 cp terraform-plan s3://zlifecycle-tfplan-zmart/$team_name/$env_name/$config_name/tf_plans/$config_reconcile_id
+aws s3 cp terraform-plan s3://zlifecycle-tfplan-zmart/$team_name/$env_name/$config_name/tfplans/$config_reconcile_id
 
 costing_payload='{"teamName": "'$team_name'", "environmentName": "'$env_name'", "component": { "componentName": "'$config_name'", "isDeleted" : '1'  }}'
 echo $costing_payload >temp_costing_payload.json
