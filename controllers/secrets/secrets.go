5b605d96fb764aba0ea6114a94559c70314ec68f
Dejan Zele Pejchev <pejcev.dejan@gmail.com>
2021-08-06 02:30:13 +0200

    Add support for scoped secrets (#81)
    Co-authored-by: Dejan Zele Pejchev <dejan@compuzest.com>

---
+++ controllers/secrets/secrets.go
@@ -0,0 +1,40 @@
+package secrets
+
+import (
+	"errors"
+	"fmt"
+)
+
+type SecretMeta struct {
+	Company              string
+	Team                 string
+	Environment          string
+	EnvironmentComponent string
+}
+
+func CreateKey(name string, scope string, meta SecretMeta) (string, error) {
+	if meta.Company == "" {
+		return "", errors.New("missing company name in secret meta")
+	}
+	switch scope {
+	case "org":
+		return fmt.Sprintf("/%s/%s", meta.Company, name), nil
+	case "team":
+		if meta.Team == "" {
+			return "", errors.New("missing team name in secret meta")
+		}
+		return fmt.Sprintf("/%s/%s/%s", meta.Company, meta.Team, name), nil
+	case "environment":
+		if meta.Team == "" || meta.Environment == "" {
+			return "", errors.New("missing team/environment name in secret meta")
+		}
+		return fmt.Sprintf("/%s/%s/%s/%s", meta.Company, meta.Team, meta.Environment, name), nil
+	case "component":
+		if meta.Team == "" || meta.Environment == "" || meta.EnvironmentComponent == "" {
+			return "", errors.New("missing team/environment/component name in secret meta")
+		}
+		return fmt.Sprintf("/%s/%s/%s/%s/%s", meta.Company, meta.Team, meta.Environment, meta.EnvironmentComponent, name), nil
+	default:
+		return "", fmt.Errorf("invalid scope: %s", scope)
+	}
+}
