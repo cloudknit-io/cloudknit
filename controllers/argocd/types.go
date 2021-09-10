70fd977714ae18096b08cac9e4a3e89ad1f052de
Dejan Zele Pejchev <pejcev.dejan@gmail.com>
2021-09-10 06:10:21 +0200

    Feature/projects (#90)
    * Add support for argocd projects
    
    * Add changes for grouping of argo apps into projects
    
    * Add support for rbac generation
    
    * Make permissions omitempty in team crd
    
    Co-authored-by: Dejan Zele Pejchev <dejan@compuzest.com>

--- controllers/argocd/types.go
+++ controllers/argocd/types.go
@@ -74,3 +74,39 @@ type Repository struct {
 	Repo string `json:"repo"`
 	Name string `json:"name"`
 }
+
+/****************************/
+/*           RBAC           */
+/****************************/
+type EntryIdentifier = string
+type Permission      = string
+
+const (
+	Policy EntryIdentifier = "p"
+	Group  EntryIdentifier = "g"
+)
+
+const (
+	Allow Permission = "allow"
+	Deny  Permission = "deny"
+)
+
+type RbacPolicy struct {
+	Identifier EntryIdentifier
+	Subject    string
+	Resource   string
+	Action     string
+	Object     string
+	Permission Permission
+}
+
+type RbacGroup struct {
+	Identifier EntryIdentifier
+	Group      string
+	Role       string
+}
+
+type RbacMap struct {
+	Policies map[string][]*RbacPolicy
+	Groups   map[string][]*RbacGroup
+}
