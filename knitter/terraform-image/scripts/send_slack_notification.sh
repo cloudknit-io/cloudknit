3f52b3a0e3bc4a83fe2a385bc3d6da0f0c15553d
Adarsh Shah <github@shahadarsh.com>
2021-10-15 18:25:00 -0400

    commenting slackk notification for now

--- knitter/terraform-image/scripts/send_slack_notification.sh
+++ knitter/terraform-image/scripts/send_slack_notification.sh
@@ -19,11 +19,11 @@ config_name=$4
 
 team_env_config_name=$team_name-$env_name-$config_name
 
-namespace="argocd"
+#namespace="argocd"
 
-message="${team_env_config_name} terraform is out of sync. To see the diff & approve the sync to desired state go here: http://localhost:8081/workflows/${namespace}/${workflow_name}"
+#message="${team_env_config_name} terraform is out of sync. To see the diff & approve the sync to desired state go here: http://localhost:8081/workflows/${namespace}/${workflow_name}"
 
-data='{"channel": "slack-notification","message": "'$message'"}'
-echo $data
+#data='{"channel": "slack-notification","message": "'$message'"}'
+#echo $data
 
-curl -d "${data}" -H "Content-Type: application/json" -X POST http://terraform-diff-eventsource-svc.${namespace}.svc.cluster.local:12000/terraform-diff
+#curl -d "${data}" -H "Content-Type: application/json" -X POST http://terraform-diff-eventsource-svc.${namespace}.svc.cluster.local:12000/terraform-diff
