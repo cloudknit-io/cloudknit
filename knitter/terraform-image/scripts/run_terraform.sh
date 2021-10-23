39875efde2b4e0af642ecf09110dbfc71df9c7f1
Shashank Sharma <shashank9226@gmail.com>
2021-10-23 23:38:41 +0530

    patching env status and adding logs to audit view

--- knitter/terraform-image/scripts/run_terraform.sh
+++ knitter/terraform-image/scripts/run_terraform.sh
@@ -40,7 +40,7 @@ function PatchError() {
   fi
   
   argocd app patch $team_env_name --patch $data --type merge > null
-  sh /audit.sh $team_name $env_name $config_name "" "Failed" $reconcile_id $config_reconcile_id $is_destroy 0
+  sh /audit.sh $team_name $env_name $config_name "Failed" "Failed" $reconcile_id $config_reconcile_id $is_destroy 0
 }
 
 function appendLogs() {
@@ -116,7 +116,8 @@ then
     then
       data='{"metadata":{"labels":{"component_status":"plan_failed"}}}'
       argocd app patch $team_env_config_name --patch $data --type merge >null
-      Error "Failed to retreive AWS Credentials."
+      echo "No AWS Credentials available. Please set AWS Credentials in the Settings Page." 2>&1 | tee /tmp/plan_output.txt
+      SaveAndExit "No AWS Credentials available."
     fi
   fi
 fi
