0121e8ee835d94a8cebad4bb15f13f5326e0a7b5
Shashank Sharma <shashank9226@gmail.com>
2021-11-08 11:40:03 +0530

    Testing change N/A changes

--- knitter/terraform-image/scripts/terraform_apply_plan.sh
+++ knitter/terraform-image/scripts/terraform_apply_plan.sh
@@ -20,6 +20,12 @@ infracost breakdown --path . --format json --log-level=warn >>output.json
 estimated_cost=$(cat output.json | jq -r ".projects[0].breakdown.totalMonthlyCost")
 resources=$(cat output.json | jq -r ".projects[0].breakdown.resources")
 
+unsupportedResources=$(cat output.json | jq ' .projects[0].summary.unsupportedResourceCounts == "{}"')
+if [[ $unsupportedResources != "true" && $estimated_cost == '0' ]]
+then
+    estimated_cost='-1'
+fi
+
 costing_payload='{"teamName": "'$team_name'", "environmentName": "'$env_name'", "component": { "componentName": "'$config_name'", "cost": '$estimated_cost', "resources" : '$resources'  }}'
 echo $costing_payload >temp_costing_payload.json
 
