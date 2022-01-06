0edc121caf9f3958474c3f13cc4cb4f10733d1d0
Dejan Zele Pejchev <dejan@compuzest.com>
2022-01-06 02:50:58 +0100

    push what I have

--- internal-cli/app/cmd/root.go
+++ internal-cli/app/cmd/root.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -29,12 +14,10 @@ import (
 
 var cfgFile string
 
-var version = "v1.0.0"
-
-// RootCmd represents the base command when called without any subcommands
+// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "zl [command]",
-	Version: version,
+	Version: env.Version,
 	Short:   "zLifecycle internal CLI",
 	Long:    `zLifecycle internal CLI for administrative management and workflow executor`,
 	Run: func(cmd *cobra.Command, args []string) {
