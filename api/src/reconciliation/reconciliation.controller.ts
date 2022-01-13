51aa25c6f9d0827e6ea0e5d379ac5afff3bd24f9
Shashank Sharma <shashank9226@gmail.com>
2022-01-13 21:20:37 +0530

    Testing out from operator to see if sse fails

--- api/src/reconciliation/reconciliation.controller.ts
+++ api/src/reconciliation/reconciliation.controller.ts
@@ -1,139 +1,220 @@
-import { Body, Controller, Get, Param, Post, Sse } from '@nestjs/common'
-import { Observable, Observer } from 'rxjs';
-import { Mapper } from 'src/costing/utilities/mapper';
-import { ComponentReconcile } from 'src/typeorm/reconciliation/component-reconcile.entity';
-import { EnvironmentReconcile } from 'src/typeorm/reconciliation/environment-reconcile.entity';
-import { Notification } from 'src/typeorm/reconciliation/notification.entity';
-import { ComponentAudit } from './dtos/componentAudit.dto';
-import { EnvironmentAudit } from './dtos/environmentAudit.dto';
-import { NotificationDto } from './dtos/notification.dto';
-import { EvnironmentReconcileDto } from './dtos/reconcile.Dto';
-import { ReconciliationService } from './services/reconciliation.service'
-
+import { Body, Controller, Get, Param, Patch, Post, Sse } from "@nestjs/common";
+import { from, Observable, Observer } from "rxjs";
+import { map } from "rxjs/operators";
+import { Mapper } from "src/costing/utilities/mapper";
+import { ComponentReconcile } from "src/typeorm/reconciliation/component-reconcile.entity";
+import { EnvironmentReconcile } from "src/typeorm/reconciliation/environment-reconcile.entity";
+import { Notification } from "src/typeorm/reconciliation/notification.entity";
+import { ComponentAudit } from "./dtos/componentAudit.dto";
+import { EnvironmentAudit } from "./dtos/environmentAudit.dto";
+import { NotificationDto } from "./dtos/notification.dto";
+import { EvnironmentReconcileDto } from "./dtos/reconcile.Dto";
+import { ReconciliationService } from "./services/reconciliation.service";
 
 @Controller({
-  path: 'reconciliation/api/v1',
+  path: "reconciliation/api/v1",
 })
 export class ReconciliationController {
   constructor(private readonly reconciliationService: ReconciliationService) {}
 
-  @Post('environment/save')
+  @Post("environment/save")
   async saveEnvironment(@Body() runData: EvnironmentReconcileDto) {
     return await this.reconciliationService.saveOrUpdateEnvironment(runData);
   }
 
-  @Post('notification/save')
+  @Post("notification/save")
   async saveNotification(@Body() notification: NotificationDto) {
     return await this.reconciliationService.saveNotification(notification);
   }
 
-  @Post('component/save')
+  @Post("component/save")
   async saveComponent(@Body() runData: EvnironmentReconcileDto) {
     return await this.reconciliationService.saveOrUpdateComponent(runData);
   }
 
-  @Get('component/:id')
-  async getComponents(@Param('id') id: string): Promise<ComponentAudit[]> {
+  @Patch("component/update")
+  async patchComponent(@Body() runData: any) {
+    return await this.reconciliationService.saveOrUpdateComponent(runData);
+  }
+
+  @Get("component/:id")
+  async getComponents(@Param("id") id: string): Promise<ComponentAudit[]> {
     return await this.reconciliationService.getComponentAuditList(id);
   }
 
-  @Get('notification/seen/:id')
-  async setSeenStatusForNotification(@Param('id') id: string): Promise<void> {
-    return await this.reconciliationService.setSeenStatusForNotification(parseInt(id));
+  @Get("notification/seen/:id")
+  async setSeenStatusForNotification(@Param("id") id: string): Promise<void> {
+    return await this.reconciliationService.setSeenStatusForNotification(
+      parseInt(id)
+    );
   }
 
-  @Get('environment/:id')
-  async getEnvironments(@Param('id') id: string): Promise<EnvironmentAudit[]> {
+  @Get("environment/:id")
+  async getEnvironments(@Param("id") id: string): Promise<EnvironmentAudit[]> {
     return await this.reconciliationService.getEnvironmentAuditList(id);
   }
 
-  @Get('component/logs/:companyId/:team/:environment/:component/:id')
-  async getLogs(@Param('companyId') companyId: string, @Param('team') team: string, @Param('environment') environment: string, @Param('component') component: string, @Param('id') id: number) {
-    return await this.reconciliationService.getLogs(companyId, team, environment, component, id);
+  @Get("component/logs/:companyId/:team/:environment/:component/:id")
+  async getLogs(
+    @Param("companyId") companyId: string,
+    @Param("team") team: string,
+    @Param("environment") environment: string,
+    @Param("component") component: string,
+    @Param("id") id: number
+  ) {
+    return await this.reconciliationService.getLogs(
+      companyId,
+      team,
+      environment,
+      component,
+      id
+    );
   }
 
-  @Get('component/latestLogs/:companyId/:team/:environment/:component')
-  async getLatestLogs(@Param('companyId') companyId: string, @Param('team') team: string, @Param('environment') environment: string, @Param('component') component: string) {
-    return await this.reconciliationService.getLatestLogs(companyId, team, environment, component);
+  @Get("component/latestLogs/:companyId/:team/:environment/:component")
+  async getLatestLogs(
+    @Param("companyId") companyId: string,
+    @Param("team") team: string,
+    @Param("environment") environment: string,
+    @Param("component") component: string
+  ) {
+    return await this.reconciliationService.getLatestLogs(
+      companyId,
+      team,
+      environment,
+      component
+    );
   }
 
-  @Get('component/state-file/:companyId/:team/:environment/:component')
-  async getStateFile(@Param('companyId') companyId: string, @Param('team') team: string, @Param('environment') environment: string, @Param('component') component: string) {
-    return await this.reconciliationService.getStateFile(companyId, team, environment, component);
+  @Get("component/state-file/:companyId/:team/:environment/:component")
+  async getStateFile(
+    @Param("companyId") companyId: string,
+    @Param("team") team: string,
+    @Param("environment") environment: string,
+    @Param("component") component: string
+  ) {
+    return await this.reconciliationService.getStateFile(
+      companyId,
+      team,
+      environment,
+      component
+    );
   }
 
-  @Get('component/plan/logs/:companyId/:team/:environment/:component/:id/:latest')
-  async getPlanLogs(@Param('companyId') companyId: string, @Param('team') team: string, @Param('environment') environment: string, @Param('component') component: string, @Param('id') id: number, @Param('latest') latest: string) {
-    return await this.reconciliationService.getPlanLogs(companyId, team, environment, component, id, latest === 'true');
+  @Get(
+    "component/plan/logs/:companyId/:team/:environment/:component/:id/:latest"
+  )
+  async getPlanLogs(
+    @Param("companyId") companyId: string,
+    @Param("team") team: string,
+    @Param("environment") environment: string,
+    @Param("component") component: string,
+    @Param("id") id: number,
+    @Param("latest") latest: string
+  ) {
+    return await this.reconciliationService.getPlanLogs(
+      companyId,
+      team,
+      environment,
+      component,
+      id,
+      latest === "true"
+    );
   }
 
-  @Get('component/apply/logs/:companyId/:team/:environment/:component/:id/:latest')
-  async getApplyLogs(@Param('companyId') companyId: string, @Param('team') team: string, @Param('environment') environment: string, @Param('component') component: string, @Param('id') id: number, @Param('latest') latest: string) {
-    return await this.reconciliationService.getApplyLogs(companyId, team, environment, component, id, latest === 'true');
+  @Get(
+    "component/apply/logs/:companyId/:team/:environment/:component/:id/:latest"
+  )
+  async getApplyLogs(
+    @Param("companyId") companyId: string,
+    @Param("team") team: string,
+    @Param("environment") environment: string,
+    @Param("component") component: string,
+    @Param("id") id: number,
+    @Param("latest") latest: string
+  ) {
+    return await this.reconciliationService.getApplyLogs(
+      companyId,
+      team,
+      environment,
+      component,
+      id,
+      latest === "true"
+    );
   }
 
-  @Get('notifications/get/:companyId/:teamName')
-  async getNotifications(@Param('companyId') companyId: string, @Param('teamName') teamName: string) {
+  @Get("notifications/get/:companyId/:teamName")
+  async getNotifications(
+    @Param("companyId") companyId: string,
+    @Param("teamName") teamName: string
+  ) {
     await this.reconciliationService.getNotification(companyId, teamName);
-    return await this.reconciliationService.getAllNotification(companyId, teamName);
+    return await this.reconciliationService.getAllNotification(
+      companyId,
+      teamName
+    );
   }
 
-  @Sse('notifications/:companyId/:teamName')
-  sendNotification(@Param('companyId') companyId: string, @Param('teamName') teamName: string): Observable<MessageEvent> {
+  @Sse("notifications/:companyId/:teamName")
+  sendNotification(
+    @Param("companyId") companyId: string,
+    @Param("teamName") teamName: string
+  ): Observable<MessageEvent> {
     return new Observable((observer: Observer<MessageEvent>) => {
       this.reconciliationService.notificationStream.subscribe(
         async (notification: Notification) => {
-          if (notification.company_id !== companyId || notification.team_name !== teamName) {
+          if (
+            notification.company_id !== companyId ||
+            notification.team_name !== teamName
+          ) {
             return;
           }
           observer.next({
             data: notification,
-          })
-        },
-      )
+          });
+        }
+      );
     });
   }
 
-  @Sse('components/notify/:id')
-  notifyComponents(@Param('id') id: string): Observable<MessageEvent> {
-    return new Observable((observer: Observer<MessageEvent>) => {
-      this.reconciliationService.notifyStream.subscribe(
-        async (component: ComponentReconcile) => {
-          if (component.name !== id) {
-            return;
-          }
-          const data: ComponentAudit[] = Mapper.getComponentAuditList([component]);
-          observer.next({
-            data: data,
-          })
-        },
-      )
-    });
+  @Sse("components/notify/:id")
+  notifyComponents(@Param("id") id: string): Observable<MessageEvent> {
+    return from(this.reconciliationService.notifyStream).pipe(
+      map((component: ComponentReconcile) => {
+        if (component.name !== id) {
+          return { data: {} };
+        }
+        const data: ComponentAudit[] = Mapper.getComponentAuditList([
+          component,
+        ]);
+        return { data };
+      })
+    );
   }
 
-  @Sse('environments/notify/:id')
-  notifyEnvironments(@Param('id') id: string): Observable<MessageEvent> {
+  @Sse("environments/notify/:id")
+  notifyEnvironments(@Param("id") id: string): Observable<MessageEvent> {
     return new Observable((observer: Observer<MessageEvent>) => {
       this.reconciliationService.notifyStream.subscribe(
         async (environment: EnvironmentReconcile) => {
           if (environment.name !== id) {
             return;
           }
-          const data: EnvironmentAudit[] = Mapper.getEnvironmentAuditList([environment]);
+          const data: EnvironmentAudit[] = Mapper.getEnvironmentAuditList([
+            environment,
+          ]);
           observer.next({
             data: data,
-          })
-        },
-      )
+          });
+        }
+      );
     });
   }
 }
 
-
-
 export interface MessageEvent {
-  data: string | object
-  id?: string
-  type?: string
-  retry?: number
+  data: string | object;
+  id?: string;
+  type?: string;
+  retry?: number;
 }
