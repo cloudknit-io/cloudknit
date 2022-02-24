971a9cd71a75e6252320f90f3273b6204b351423
Shashank Sharma <shashank9226@gmail.com>
2022-02-25 00:13:12 +0530

    Adding UserEntity

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
 import { AuthController } from './controller/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      User
+    ])
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
