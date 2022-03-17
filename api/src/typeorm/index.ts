bb193f593266c7788ea8b439c44d0f7d397e2d7e
Shashank Sharma <shashank9226@gmail.com>
2022-03-17 22:28:12 +0530

    Adding company to entities

--- api/src/typeorm/index.ts
+++ api/src/typeorm/index.ts
@@ -1,6 +1,7 @@
+import { Company } from './company/Company';
 import { costingEntities } from './costing';
 import { User } from './entities/User';
 import { reconcileEntities } from './reconciliation';
 import { resourceEntities } from './resources';
 
-export const entities = [User, ...costingEntities, ...resourceEntities, ...reconcileEntities];
+export const entities = [Company, User, ...costingEntities, ...resourceEntities, ...reconcileEntities];
