aae42d1cac78e7656afd15c47feb4e4b61cb58c1
Dejan Zele Pejchev <dejan@compuzest.com>
2022-04-05 23:39:06 +0200

    v0.0.7

--- state-manager/main.go
+++ state-manager/main.go
@@ -5,7 +5,7 @@ import (
 	"github.com/compuzest/zlifecycle-state-manager/app/zlog"
 )
 
-var Version = "0.0.6"
+var Version = "0.0.7"
 
 func main() {
 	zlog.PlainLogger().WithField("version", Version).Info("Starting zlifecycle state manager on localhost:8080")
