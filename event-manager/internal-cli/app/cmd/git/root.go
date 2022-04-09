e93ab70cd892ff5da33222a85d62714c173c4fe7
Dejan Zele Pejchev <dejan@compuzest.com>
2022-04-09 05:05:22 +0200

    add git login command

--- event-manager/internal-cli/app/cmd/git/root.go
+++ event-manager/internal-cli/app/cmd/git/root.go
@@ -1,6 +1,9 @@
 package git
 
 import (
+	"fmt"
+	"github.com/compuzest/zlifecycle-internal-cli/app/common"
+	"github.com/compuzest/zlifecycle-internal-cli/app/env"
 	"github.com/spf13/cobra"
 )
 
@@ -13,5 +16,18 @@ var RootCmd = &cobra.Command{
 }
 
 func init() {
+	RootCmd.PersistentFlags().StringVarP(&env.GitAuth, "git-auth", "g", "", "Git auth method")
+	if err := RootCmd.MarkPersistentFlagRequired("git-auth"); err != nil {
+		fmt.Println(err)
+		common.Failure(3201)
+	}
+
+	RootCmd.PersistentFlags().StringVarP(&env.GitHubAppID, "app-id", "a", "", "GitHub App organization ID")
+
+	RootCmd.PersistentFlags().StringVarP(&env.GitHubAppSSHPath, "ssh", "s", "", "GitHub App private key filepath")
+
+	RootCmd.PersistentFlags().StringVarP(&env.GitToken, "token", "t", "", "Git token")
+
 	RootCmd.AddCommand(cloneCmd)
+	RootCmd.AddCommand(loginCmd)
 }
