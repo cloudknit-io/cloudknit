46be97d60f36f17ca905ddf42eb20eda20616e6f
Adarsh Shah <github@shahadarsh.com>
2022-04-10 10:53:33 -0400

    renaming ssh key

--- knitter/terraform-image/scripts/client/setup_github.sh
+++ knitter/terraform-image/scripts/client/setup_github.sh
@@ -10,6 +10,6 @@
 # strictly forbidden unless prior written permission is obtained from CompuZest, Inc.
 
 mkdir ~/.ssh
-cat /root/ssh_secret/id_rsa >> ~/.ssh/id_rsa
+cat /root/git_ssh/id_rsa >> ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
