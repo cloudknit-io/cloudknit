bae843aa487ded61f754fcceaf91ab38462c144e
dejanzele <dejan@compuzest.com>
2022-05-26 13:07:19 +0200

    fix issues

--- il-operator/controller/common/aws/awseks/service.go
+++ il-operator/controller/common/aws/awseks/service.go
@@ -2,7 +2,7 @@ package awseks
 
 import "context"
 
-//go:generate mockgen --build_flags=--mod=mod -destination=./mock_eks_api.go -package=awseks "github.com/compuzest/zlifecycle-il-operator/controller/external/aws/awseks" API
+//go:generate mockgen --build_flags=--mod=mod -destination=./mock_eks_api.go -package=awseks "github.com/compuzest/zlifecycle-il-operator/controller/common/aws/awseks" API
 type API interface {
 	DescribeCluster(ctx context.Context, name string) (*ClusterInfo, error)
 }
