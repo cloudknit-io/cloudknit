0b2b7404d752b85ff88799673f7a4fe24930e53e
Adarsh Shah <github@shahadarsh.com>
2022-11-08 21:48:22 -0500

    cleaning up old Copyright messages

--- cli/types/environment_types.go
+++ cli/types/environment_types.go
@@ -1,15 +1,3 @@
-/* Copyright (C) 2020 CompuZest, Inc. - All Rights Reserved
-*
-* Unauthorized copying of this file, via any medium, is strictly prohibited
-* Proprietary and confidential
-*
-* NOTICE: All information contained herein is, and remains the property of
-* CompuZest, Inc. The intellectual and technical concepts contained herein are
-* proprietary to CompuZest, Inc. and are protected by trade secret or copyright
-* law. Dissemination of this information or reproduction of this material is
-* strictly forbidden unless prior written permission is obtained from CompuZest, Inc.
-*/
-
 package v1alpha1
 
 import (
--- il-operator/api/v1/groupversion_info.go
+++ il-operator/api/v1/groupversion_info.go
@@ -1,15 +1,3 @@
-/* Copyright (C) 2020 CompuZest, Inc. - All Rights Reserved
- *
- * Unauthorized copying of this file, via any medium, is strictly prohibited
- * Proprietary and confidential
- *
- * NOTICE: All information contained herein is, and remains the property of
- * CompuZest, Inc. The intellectual and technical concepts contained herein are
- * proprietary to CompuZest, Inc. and are protected by trade secret or copyright
- * law. Dissemination of this information or reproduction of this material is
- * strictly forbidden unless prior written permission is obtained from CompuZest, Inc.
- */
-
 // Package v1 contains API Schema definitions for the stable v1 API group
 // +kubebuilder:object:generate=true
 // +groupName=stable.compuzest.com
