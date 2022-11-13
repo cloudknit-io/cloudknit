8d47b877ebeccc6f0764d170e01f3f77dae75bb3
Shashank Sharma <shashank9226@gmail.com>
2022-11-13 21:11:13 +0530

    Non Italicising text docs link to _blank

--- web/src/pages/authorized/quick-start/guides/ConfigureZlifecycle.tsx
+++ web/src/pages/authorized/quick-start/guides/ConfigureZlifecycle.tsx
@@ -62,7 +62,7 @@ export class ConfiguringZlifecycle extends BaseGuide implements IGuide {
 							<form ref={formRef} className={`${cls('_form')}`}>
 								<h6 className={`${cls('_heading')}`}>Step 1.</h6>
 								<section className={`${cls('_form-group')}`}>
-									<em>Create a new public or private repo in your github org.</em>
+									<span>Create a new public or private repo in your github org.</span>
 								</section>
 								<h6 className={`${cls('_heading')}`}>Step 2.</h6>
 								<section className={`${cls('_form-group')}`}>
@@ -95,14 +95,14 @@ export class ConfiguringZlifecycle extends BaseGuide implements IGuide {
 								</section>
 								<h6 className={`${cls('_heading')}`}>Step 3. (Only required for private repo)</h6>
 								<section className={`${cls('_form-group')}`}>
-									<em>
-										Provide CloudKnit access to the github repo by following steps provided <a href="https://docs.cloudknit.io/getting_started/install_github_app/">here</a>
-									</em>
+									<span>
+										Provide CloudKnit access to the github repo by following steps provided <a href="https://docs.cloudknit.io/getting_started/install_github_app/" target='_blank'>here</a>
+									</span>
 									<label></label>
 									<label>OR</label>
-									<em>
-										<a href="mailto:contact.cloudknit.io">Contact us</a> for other alternative.
-									</em>
+									<span>
+										<a href="mailto:contact@cloudknit.io">Contact us</a> for other alternative.
+									</span>
 								</section>
 							</form>
 						</div>
