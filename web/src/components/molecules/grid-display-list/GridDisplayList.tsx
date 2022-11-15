dd360e3011257f8a6e3139f877feb132170a2748
Shashank <83878475+shashank-cloudknit-io@users.noreply.github.com>
2022-11-15 21:06:34 +0530

    Fix/cleanup web (#184)
    * removing dead code
    
    * removing unwanted code
    
    Co-authored-by: Shashank Sharma <shashank9226@gmail.com>

--- web/src/components/molecules/grid-display-list/GridDisplayList.tsx
+++ web/src/components/molecules/grid-display-list/GridDisplayList.tsx
@@ -9,42 +9,6 @@ type Props = {
 	items: ListItem[];
 };
 
-const renderTextValue = (value: string | undefined, index: number) => {
-	return (
-		<div key={`display-grid-text-value${index}`} className="zlifecycle-grid-display-list__value">
-			{!value || value === '' ? (
-				<ZText.Body className="zlifecycle-grid-display-list__value--muted" size="14" lineHeight="18">
-					No data available
-				</ZText.Body>
-			) : (
-				<ZTooltip content={value}>
-					<span>
-						<ZText.Body key={'list-item-value-' + value + index} size="14" lineHeight="18">
-							{value}
-						</ZText.Body>
-					</span>
-				</ZTooltip>
-			)}
-		</div>
-	);
-};
-
-export const ZGridDisplayList: FC<Props> = ({ items }: Props) => {
-	return (
-		<div className="zlifecycle-grid-display-list">
-			{items.map((item, index) => (
-				<div key={'display-grid-' + Math.random()}>
-					{!item.value || typeof item.value === 'string' ? (
-						renderTextValue(item.value as any, index)
-					) : (
-						<div className="zlifecycle-grid-display-list__value">{item.value}</div>
-					)}
-				</div>
-			))}
-		</div>
-	);
-};
-
 const renderTextValueWithLabel = (value: string | undefined, index: number) => {
 	return (
 		<div key={`display-grid-label-text-value${index}`} className="zlifecycle-grid-display-list__value">
