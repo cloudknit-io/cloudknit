4bc7fe490d78ecc202ea8e29ea6a008c472a261b
Brad Janke <bradj@users.noreply.github.com>
2022-11-17 08:49:05 -0600

    adds org update to IsProvisioned operation

--- api/src/operations/operations.controller.ts
+++ api/src/operations/operations.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Request } from '@nestjs/common';
+import { OrganizationService } from 'src/organization/organization.service';
 import { OperationsService } from './operations.service';
 
 @Controller({
@@ -6,13 +7,19 @@ import { OperationsService } from './operations.service';
 })
 export class OperationsController {
   constructor(
-    private readonly opsService: OperationsService
+    private readonly opsService: OperationsService,
+    private readonly orgService: OrganizationService
   ){}
 
   @Get("/is-provisioned")
   public async check(@Request() req) {
     const isProvisioned = await this.opsService.isOrgProvisioned(req.org);
 
-    return { provisioned: isProvisioned };
+    if (isProvisioned === true) {
+      const org = await this.orgService.patchOrganization(req.org, {provisioned: true});
+      return org;
+    }
+
+    return req.org;
   }
 }
--- api/src/operations/operations.module.ts
+++ api/src/operations/operations.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrganizationService } from 'src/organization/organization.service';
 import { Organization } from 'src/typeorm';
 import { OperationsController } from './operations.controller';
 import { OperationsService } from './operations.service';
@@ -7,6 +8,6 @@ import { OperationsService } from './operations.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Organization])],
   controllers: [OperationsController],
-  providers: [OperationsService]
+  providers: [OperationsService, OrganizationService]
 })
 export class OperationsModule {}
