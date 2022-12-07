5c876c0896e73ab505e5a14a6601511e2b533b47
Shashank Sharma <shashank9226@gmail.com>
2022-12-07 21:10:02 +0530

    Breadcrumb Route and redirection Fixes

--- web/src/pages/authorized/breadcrumb/Breadcrumbs.tsx
+++ web/src/pages/authorized/breadcrumb/Breadcrumbs.tsx
@@ -32,22 +32,6 @@ export const Breadcrumbs: FC = () => {
 				break;
 			case FilterRegion.ENVIRONMENT:
 				filter = LocalStorage.getItem<[]>(LocalStorageKey.ENVIRONMENTS) || [];
-				type = history.location.pathname.split('/').slice(-1)[0];
-				break;
-			case FilterRegion.COMPONENTS:
-				const subPath = '/' + tokens.slice(1, FilterRegion.COMPONENTS).join('/');
-				filter = [
-					{
-						name: 'Infra',
-						path: subPath + '/infra',
-					},
-				];
-				if (FeatureRoutes[FeatureKeys.APPLICATIONS]) {
-					filter.push({
-						name: 'Apps',
-						path: subPath + '/apps',
-					});
-				}
 				break;
 			default:
 				filter = [];
