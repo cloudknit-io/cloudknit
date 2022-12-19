5ed98d308b1601a79e67295348d037ff0b2e8d86
Shashank Sharma <shashank9226@gmail.com>
2022-12-19 23:14:30 +0530

    removing typo

--- api/src/costing/services/component.service.ts
+++ api/src/costing/services/component.service.ts
@@ -175,7 +175,7 @@ export class ComponentService {
       component.teamName = costing.teamName;
       component.environment = env;
       component.id = id;
-      component.status = costing.component.status || ;
+      component.status = costing.component.status;
       component.componentName = costing.component.componentName;
       component.cost = costing.component.cost;
       component.isDestroyed = costing.component.isDestroyed;
