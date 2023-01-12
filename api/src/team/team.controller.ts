b3e37a03b99487def8787d80f93f5bcb3705ab39
Brad Janke <bradj@users.noreply.github.com>
2023-01-12 08:31:04 -0600

    adds api query params to swagger spec for teams controller

--- api/src/team/team.controller.ts
+++ api/src/team/team.controller.ts
@@ -10,6 +10,7 @@ import { TeamQueryParams } from './team.dto';
 import { TeamWrapDto } from './dto/team-cost.dto';
 import { calculateTeamCost } from './team.helper';
 import { Team } from 'src/typeorm';
+import { ApiQuery } from '@nestjs/swagger';
 
 @Controller({
   version: '1'
@@ -71,7 +72,9 @@ export class TeamController {
 
   @Get()
   @OrgApiParam()
-  async findAll(@Request() req, @Query() qParams: TeamQueryParams): Promise<TeamWrapDto[]> {
+  @ApiQuery({name: 'withCost', required: false, type: 'boolean'})
+  @ApiQuery({name: 'withEnvironments', required: false, type: 'boolean'})
+  async findAll(@Request() req: APIRequest, @Query() qParams: TeamQueryParams): Promise<TeamWrapDto[]> {
     const org = req.org;
     const withCost = qParams.withCost.toLowerCase() === 'true';
     const withEnv = qParams.withEnvironments.toLowerCase() === 'true';
