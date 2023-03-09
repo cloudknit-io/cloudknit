9d25ca53fdaa039acabf09a246271bba9651f549
Adarsh Shah <github@shahadarsh.com>
2023-03-08 22:45:03 -0500

    calls errors endpoint in api to post errors instead of event svc

--- il-operator/controller/common/cloudknitservice/environment.go
+++ il-operator/controller/common/cloudknitservice/environment.go
@@ -15,6 +15,11 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type PostError struct {
+	EnvName      string   `json:"envName"`
+	ErrorMessage []string `json:"errorMessage"`
+}
+
 func (s *Service) PostEnvironment(ctx context.Context, organizationName string,
 	environment stablev1.Environment, log *logrus.Entry) error {
 	endpoint := fmt.Sprintf("%s/%s/%s/%s/%s/%s", s.host,
@@ -56,6 +61,57 @@ func (s *Service) PostEnvironment(ctx context.Context, organizationName string,
 	log.
 		Infof(
 			"Successful response from CloudKnitService for POST Environment request for Environment: %s",
+			environment.Spec.EnvName,
+		)
+
+	return nil
+}
+
+func (s *Service) PostError(ctx context.Context, organizationName string,
+	environment *stablev1.Environment, allErrs []string, log *logrus.Entry) error {
+	endpoint := fmt.Sprintf("%s/%s/%s/%s/%s/%s", s.host,
+		"v1/orgs", organizationName, "teams",
+		environment.Spec.TeamName, "errors")
+
+	log.
+		Infof(
+			"CloudKnitService Endpoint: %s",
+			endpoint,
+		)
+
+	log.
+		Infof(
+			"Error Post Call via CloudKnitService for Env: %s",
+			environment.Spec.EnvName,
+		)
+
+	jsonBody, err := util.ToJSON(PostError{
+		EnvName:      environment.Spec.EnvName,
+		ErrorMessage: allErrs,
+	})
+
+	if err != nil {
+		return errors.Wrap(err, "error marshaling Errors body")
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return errors.Wrap(err, "error creating POST Error request")
+	}
+	req.Header.Add("Content-Type", runtime.ContentTypeJSON)
+	resp, err := s.httpClient.Do(req)
+	if err != nil {
+		return errors.Wrap(err, "error executing POST Error request")
+	}
+	defer util.CloseBody(resp.Body)
+
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		return errors.Errorf("POST Error returned a non-OK status code: [%d]", resp.StatusCode)
+	}
+
+	log.
+		Infof(
+			"Successful response from CloudKnitService for POST Errors request for Environment: %s",
 			environment.Spec.EnvName,
 		)
 
