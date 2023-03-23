a6632ebc5527da6dfa997dc3d8dea6a085f553ae
Shashank Sharma <shashank9226@gmail.com>
2023-03-23 20:15:36 +0530

    Adding usage of session token

--- knitter/terraform-image/scripts/client/setup_aws.sh
+++ knitter/terraform-image/scripts/client/setup_aws.sh
@@ -6,4 +6,5 @@ aws_secret_access_key = ${CUSTOMER_AWS_SECRET_ACCESS_KEY}
 [compuzest-shared]
 aws_access_key_id = ${SHARED_AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${SHARED_AWS_SECRET_ACCESS_KEY}
+aws_session_token = ${SHARED_AWS_SESSION_TOKEN}
 EOT
