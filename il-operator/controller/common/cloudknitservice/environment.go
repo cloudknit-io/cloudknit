0da610fc038f4afb974fd0cd9bd6d4d8554472e1
Adarsh Shah <github@shahadarsh.com>
2023-04-12 10:13:44 -0400

    adding env patch call for reconciliation changes in operator when IL changes

--- il-operator/controller/common/cloudknitservice/environment.go
+++ il-operator/controller/common/cloudknitservice/environment.go
@@ -21,6 +21,10 @@ type PostError struct {
 	Status       string   `json:"status"`
 }
 
+type PatchEnvironment struct {
+	IsReconcile bool `json:"isReconcile"`
+}
+
 func (s *Service) PostEnvironment(ctx context.Context, organizationName string,
 	environment stablev1.Environment, log *logrus.Entry) error {
 	endpoint := fmt.Sprintf("%s/%s/%s/%s/%s/%s", s.host,
@@ -62,6 +66,55 @@ func (s *Service) PostEnvironment(ctx context.Context, organizationName string,
 	log.
 		Infof(
 			"Successful response from CloudKnitService for POST Environment request for Environment: %s",
+			environment.Spec.EnvName,
+		)
+
+	return nil
+}
+
+func (s *Service) PatchEnvironment(ctx context.Context, organizationName string,
+	environment stablev1.Environment, log *logrus.Entry) error {
+	endpoint := fmt.Sprintf("%s/%s/%s/%s/%s/%s/%s", s.host,
+		"v1/orgs", organizationName, "teams",
+		environment.Spec.TeamName, "environments", environment.Name)
+
+	log.
+		Infof(
+			"CloudKnitService Endpoint: %s",
+			endpoint,
+		)
+
+	log.
+		Infof(
+			"Environment PAtch Call via CloudKnitService for Env: %s",
+			environment.Spec.EnvName,
+		)
+
+	jsonBody, err := util.ToJSON(PatchEnvironment{
+		IsReconcile: true,
+	})
+	if err != nil {
+		return errors.Wrap(err, "error marshaling Patch Environment body")
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPatch, endpoint, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return errors.Wrap(err, "error creating PATCH Environment request")
+	}
+	req.Header.Add("Content-Type", runtime.ContentTypeJSON)
+	resp, err := s.httpClient.Do(req)
+	if err != nil {
+		return errors.Wrap(err, "error executing PATCH Environment request")
+	}
+	defer util.CloseBody(resp.Body)
+
+	if resp.StatusCode != 200 && resp.StatusCode != 201 {
+		return errors.Errorf("PATCH Environment returned a non-OK status code: [%d]", resp.StatusCode)
+	}
+
+	log.
+		Infof(
+			"Successful response from CloudKnitService for PATCH Environment request for Environment: %s",
 			environment.Spec.EnvName,
 		)
 
