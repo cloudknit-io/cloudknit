5a60baf76b144593b65bd67518688ecf6b63f17d
Shashank <83878475+shashank-cloudknit-io@users.noreply.github.com>
2023-04-21 18:35:49 +0530

    adds session token support in operator (#307)

--- il-operator/controller/util/aws.go
+++ il-operator/controller/util/aws.go
@@ -3,6 +3,7 @@ package util
 const (
 	AWSAccessKeyID       = "aws_access_key_id"     //nolint
 	AWSSecretAccessKey   = "aws_secret_access_key" //nolint
+	AWSSessionToken      = "aws_session_token"     //nolint
 	StateBucketSecret    = "state_bucket"
 	StateLockTableSecret = "state_lock_table"
 )
