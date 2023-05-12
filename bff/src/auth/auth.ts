493ea182f7a59a51727c1f0ff9e40c57957c71d9
Shashank <83878475+shashank-cloudknit-io@users.noreply.github.com>
2023-05-12 18:17:10 +0530

    Update auth.ts

--- bff/src/auth/auth.ts
+++ bff/src/auth/auth.ts
@@ -270,7 +270,7 @@ export function setUpAuth(app: express.Express, authRouter: express.Router) {
     app.use(getOktaAuthMW());
 
     authRouter.get("/auth/logout", (req: any, res: any, next: any) => {
-      console.log('ip->test-->', req.ip, req.socket.remoteAddress);
+      console.log('ip->test-->', req.ip, req.socket.remoteAddress, req.headers['x-forwarded-for'], req.connection.remoteAddress);
       req.logout(function (err) {
         if (err) {
           return next(err);
