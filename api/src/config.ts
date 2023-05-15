b1643c061b0c7d57898cd6b57c13e19eeb61685f
Shashank <83878475+shashank-cloudknit-io@users.noreply.github.com>
2023-05-15 21:24:21 +0530

    adds TypeOrm synchronize as a flag to sync it when required (#315)

--- api/src/config.ts
+++ api/src/config.ts
@@ -7,6 +7,7 @@ export type ApiConfig = {
     username: string;
     password: string;
     database: string;
+    sync: boolean;
   };
   port: number;
   AWS: {
@@ -60,6 +61,7 @@ export function init() {
       username: getEnvVarOrFail('TYPEORM_USERNAME'),
       password: getEnvVarOrFail('TYPEORM_PASSWORD'),
       database: getEnvVarOrFail('TYPEORM_DATABASE'),
+      sync: Boolean(getEnvVarOrFail('TYPEORM_SYNC'))
     },
     port: parseInt(process.env.APP_PORT) || 3000,
     AWS: {
