a5a16d8975647cefb0cb70b00b4b69f53a0d471d
Shashank Sharma <shashank9226@gmail.com>
2023-05-19 22:41:42 +0530

    Removing token from code

--- api/src/config.ts
+++ api/src/config.ts
@@ -68,7 +68,7 @@ export function init() {
     },
     port: parseInt(process.env.APP_PORT) || 3000,
     github: {
-      personalAccessToken: getEnvVarOrDefault('GIT_PERSONAL_ACCESS_TOKEN', 'Z2hwX3R3bjg0NVdVU3dOMHg2S2JsbjZ2TFJpZlBTeWJRcDNvQ1pjMQ=='), //getEnvVarOrFail('GIT_PERSONAL_ACCESS_TOKEN'),
+      personalAccessToken: getEnvVarOrDefault('GIT_PERSONAL_ACCESS_TOKEN', ''), //getEnvVarOrFail('GIT_PERSONAL_ACCESS_TOKEN'),
       owner: getEnvVarOrDefault('GIT_OWNER', 'zlab-tech'),
       repo: getEnvVarOrDefault('GIT_REPO', 'hooli-config'),
     },
