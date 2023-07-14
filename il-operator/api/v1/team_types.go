fde38ed358f73c630004c5b27b7dd77bd90718e8
Shashank Sharma <shashank9226@gmail.com>
2023-07-14 18:16:56 +0530

    Adding omitempty to teardownProtection

--- il-operator/api/v1/team_types.go
+++ il-operator/api/v1/team_types.go
@@ -25,7 +25,7 @@ type Team struct {
 type TeamSpec struct {
 	TeamName           string   `json:"teamName"`
 	ConfigRepo         *Repo    `json:"configRepo"`
-	TeardownProtection bool     `json:"teardownProtection"`
+	TeardownProtection bool     `json:"teardownProtection,omitempty"`
 	Permissions        []string `json:"permissions,omitempty"`
 }
 
