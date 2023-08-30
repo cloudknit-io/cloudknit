dfdf62198c5839e8efb9ab04d19297fded2b2c2a
Shashank Sharma <shashank9226@gmail.com>
2023-08-30 10:30:26 +0530

    Sending empty stream to keep the connection alive.

--- bff/src/sse/sse.ts
+++ bff/src/sse/sse.ts
@@ -56,7 +56,7 @@ async function eventsHandler(request: BFFRequest, response: any, next) {
   response.writeHead(200, headers);
 
   event.on("stream", (stream: { data: any; type: string }) => {
-    if (selectedOrg?.id !== stream.data.orgId) return;
+    if (selectedOrg?.id !== stream.data.orgId && stream.type !== "Empty") return;
     response.write(
       `event: ${stream.type}\ndata: ${JSON.stringify(stream.data)}\n\n`
     );
@@ -69,4 +69,11 @@ async function eventsHandler(request: BFFRequest, response: any, next) {
 
 export function setUpSSE(router: express.Router) {
   router.get("/session/stream", eventsHandler);
+  // This is done keep the event source alive
+  setInterval(() => {
+    event.emit("stream", {
+      data: {},
+      type: "Empty",
+    });
+  }, 10000);
 }
