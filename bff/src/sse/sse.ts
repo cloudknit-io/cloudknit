8dd5607c639aaba744c484e4677bd67121190acf
Shashank <83878475+shashank-cloudknit-io@users.noreply.github.com>
2023-09-01 08:24:54 +0530

    sends empty stream to keep the connection alive (#348)

--- bff/src/sse/sse.ts
+++ bff/src/sse/sse.ts
@@ -9,6 +9,7 @@ import { BFFRequest } from "../types";
 const event = new EventEmitter();
 let redisClient = null;
 const apiStreamChannel = "api-stream-channel";
+let interval = null;
 
 async function startRedis() {
   try {
@@ -56,17 +57,25 @@ async function eventsHandler(request: BFFRequest, response: any, next) {
   response.writeHead(200, headers);
 
   event.on("stream", (stream: { data: any; type: string }) => {
-    if (selectedOrg?.id !== stream.data.orgId) return;
+    if (selectedOrg?.id !== stream.data.orgId && stream.type !== "Empty") return;
     response.write(
       `event: ${stream.type}\ndata: ${JSON.stringify(stream.data)}\n\n`
     );
   });
 
   request.on("close", () => {
+    if (interval) clearInterval(interval);
     console.log(`Connection closed`);
   });
 }
 
 export function setUpSSE(router: express.Router) {
   router.get("/session/stream", eventsHandler);
+  // This is done keep the event source alive
+  interval = setInterval(() => {
+    event.emit("stream", {
+      data: {},
+      type: "Empty",
+    });
+  }, 10000);
 }
