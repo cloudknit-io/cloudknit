449ba6181c3f7df7e6c188a142b00a992121b421
Shashank Sharma <shashank9226@gmail.com>
2023-09-05 21:47:09 +0530

    event closing on request closure.

--- bff/src/sse/sse.ts
+++ bff/src/sse/sse.ts
@@ -9,7 +9,6 @@ import { BFFRequest } from "../types";
 const event = new EventEmitter();
 let redisClient = null;
 const apiStreamChannel = "api-stream-channel";
-let interval = null;
 
 async function startRedis() {
   try {
@@ -23,6 +22,13 @@ async function startRedis() {
     await redisClient.connect();
 
     if (redisClient.isReady) {
+      // This is done keep the event source alive
+      setInterval(() => {
+        event.emit("stream", {
+          data: {},
+          type: "Empty",
+        });
+      }, 10000);
       redisClient.SUBSCRIBE(apiStreamChannel, (message: any, channel: any) => {
         event.emit("stream", JSON.parse(message));
       });
@@ -57,25 +63,18 @@ async function eventsHandler(request: BFFRequest, response: any, next) {
   response.writeHead(200, headers);
 
   event.on("stream", (stream: { data: any; type: string }) => {
-    if (selectedOrg?.id !== stream.data.orgId && stream.type !== "Empty") return;
+    if (selectedOrg?.id !== stream.data.orgId && stream.type !== "Empty")
+      return;
     response.write(
       `event: ${stream.type}\ndata: ${JSON.stringify(stream.data)}\n\n`
     );
   });
 
   request.on("close", () => {
-    if (interval) clearInterval(interval);
     console.log(`Connection closed`);
   });
 }
 
 export function setUpSSE(router: express.Router) {
   router.get("/session/stream", eventsHandler);
-  // This is done keep the event source alive
-  interval = setInterval(() => {
-    event.emit("stream", {
-      data: {},
-      type: "Empty",
-    });
-  }, 10000);
 }
